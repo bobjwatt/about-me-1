8094359505bf37036907168db65d367560646ae5
TartarSauce <uma_bangalore@hotmail.com>
2016-05-10 14:17:57 -0700

    skeleton of HTML,first JSquestion, CSS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// this is a comment
+// var userName = prompt('Hi, Thanks for visiting my site!, what is your name');
+//
+// alert('Nice to meet you,' + userName + '. I\'m going you ask you some questions about me.');
+// console.log(userName);
+
+var userName = 'uma';
+
+var response1 = prompt('Do I have three cats?');
+var answer1 = 'yes';
+
+if (response1.toLowerCase() === answer1) {
+  alert('You are correct, ' + userName);
+} else {
+  alert('You are not correct, I do have three cats');
+}
