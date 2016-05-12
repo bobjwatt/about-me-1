455e69a983976503e09b7f4422dd9a766d97393f
Bob <bobjwatt@gmail.com>
2016-05-12 14:29:20 -0700

    added funtions and updated readme

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Added question 6, the guessing game.
 Added question 7, the guessing game of which state I am from.
 
 Added user performance statistics
+
+Bob's changes to Uma's repo
--- app.js
+++ app.js
@@ -21,145 +21,165 @@ if (!turnOffPrompts) {
   //------------------------
   // First question to user
   //------------------------
-  var response1 = prompt('Do I have a dog?').toLowerCase();
-  var answer1 = 'yes';
+  function checkPet(){
+    var response1 = prompt('Do I have a dog?').toLowerCase();
+    var answer1 = 'yes';
+    if (response1 === answer1 || response1 === answer1[0]) {
+      alert('You are correct, ' + userName);
+      finalTally++;
+    } else {
+      alert('You are not correct, ' + userName + '. I do have a dog');
+    }
+  }
+  checkPet();
   var petImage = document.getElementById('pet');
   var petBorder = false;
-
-  if (response1 === answer1 || response1 === answer1[0]) {
-    alert('You are correct, ' + userName);
-    finalTally++;
-  } else {
-    alert('You are not correct, ' + userName + '. I do have a dog');
-  }
   petImage.src = 'imgs/Sayla.jpg';
   petBorder = true;
   //------------------------
   // Second question to user
   //------------------------
-  var response2 = prompt('Where do I live? Hint: its a popular city in the Northwest.');
-  var answer2 = 'seattle';
+  function homeTown(){
+    var response2 = prompt('Where do I live? Hint: its a popular city in the Northwest.');
+    var answer2 = 'seattle';
+
+    if (response2.toLowerCase() === answer2) {
+      alert('Yes beautiful Seattle, that\'s my hometown, ' + userName + '.');
+      finalTally++;
+    } else {
+      alert('No, sorry that is not correct. I am from Seattle in Washington.');
+    }
+  }
+  homeTown();
   var homeTownImage = document.getElementById('hometown');
   var homeTownBorder = false;
-
-  if (response2.toLowerCase() === answer2) {
-    alert('Yes beautiful Seattle, that\'s my hometown, ' + userName + '.');
-    finalTally++;
-  } else {
-    alert('No, sorry that is not correct. I am from Seattle in Washington.');
-  }
   homeTownImage.src = 'imgs/seattle.jpg';
   homeTownBorder = true;
   //------------------------
   // Third question to user
   //------------------------
-  var response3 = prompt('What\'s my favorite food? Hint: think cheese and pepperoni.').toLowerCase();
-  var answer3 = 'pizza';
+  function faveFood() {
+    var response3 = prompt('What\'s my favorite food? Hint: think cheese and pepperoni.').toLowerCase();
+    var answer3 = 'pizza';
+
+    if (response3 === answer3) {
+      alert('Yay! that\'s correct, ' + userName + '. I love pizza.');
+      finalTally++;
+    } else {
+      alert('No, sorry that is not correct. I love eating pizza.');
+    }
+  }
+  faveFood();
   var foodImage = document.getElementById('food');
   var foodBorder = false;
-
-  if (response3 === answer3) {
-    alert('Yay! that\'s correct, ' + userName + '. I love pizza.');
-    finalTally++;
-  } else {
-    alert('No, sorry that is not correct. I love eating pizza.');
-  }
   foodImage.src = 'imgs/pizza.png';
   foodBorder = true;
   //------------------------
   // Fourth question to user
   //------------------------
-  var response4 = prompt('What\'s the scariest movie I watched? Hint: ghost that creates physical havoc.').toLowerCase();
-  var answer4 = 'poltergeist';
+  function scaryMovie() {
+    var response4 = prompt('What\'s the scariest movie I watched? Hint: ghost that creates physical havoc.').toLowerCase();
+    var answer4 = 'poltergeist';
+
+    if (response4 === answer4) {
+      alert('That movie gives me the creeps. Good guess, ' + userName + '.');
+      finalTally++;
+    } else {
+      alert('Actually, the movie that scared me the most recently was Poltergeist.');
+    }
+  }
+  scaryMovie();
   var movieImage = document.getElementById('movie');
   var movieBorder = false;
-
-  if (response4 === answer4) {
-    alert('That movie gives me the creeps. Good guess, ' + userName + '.');
-    finalTally++;
-  } else {
-    alert('Actually, the movie that scared me the most recently was Poltergeist.');
-  }
   movieImage.src = 'imgs/movie.jpg';
   movieBorder = true;
   //------------------------
   // Fifth question to user
   //------------------------
-  var response5 = prompt('What\'s my favorite place to be Hint: its where the heart is.').toLowerCase();
-  var answer5 = 'home';
+  function favePlace() {
+    var response5 = prompt('What\'s my favorite place to be Hint: its where the heart is.').toLowerCase();
+    var answer5 = 'home';
+
+    if (response5 === answer5) {
+      alert('Yes, nothing like a staycation, ' + userName + '.');
+      finalTally++;
+    } else {
+      alert('No, I usually like to laze around at home.');
+    }
+  }
+  favePlace();
   var favPlaceImage = document.getElementById('favplace');
   var favPlaceBorder = false;
-
-  if (response5 === answer5) {
-    alert('Yes, nothing like a staycation, ' + userName + '.');
-    finalTally++;
-  } else {
-    alert('No, I usually like to laze around at home.');
-  }
   favPlaceImage.src = 'imgs/home.jpg';
   favPlaceBorder = true;
   //------------------------
   // sixth question to user
   //------------------------
-  var answer6 = 1 + Math.floor(Math.random() * 10); // random number between 1 and 10
-  console.log('This is my random number between 1 and 10: ' + answer6);
-  var numTries = 0;
-  var doneGame = false;
-  alert('Let\'s play a game. I am thinking of an integer between'
-                                + '1 and 10. You have exactly four guesses to get the correct '
-                                + 'answer. Let\'s go!');
-  while(numTries < 4) {
-    var response6 = parseInt(prompt('Type in your number'));
-    numTries++;
-    if (response6 === answer6) {
-      alert('You guessed right. That\'s awesome! Thank you for playing my game');
-      doneGame = true;
-      finalTally++;
-      break; // break out of the while loop
-    } else {
-      if (response6 > answer6) {
-        alert('Sorry, guess a lower number. You have ' + (4 - numTries) + ' tries left.');
-      }
-      else {
-        alert('Sorry, guess a higher number. You have ' + (4 - numTries) + ' tries left.');
+  function guessNumber() {
+    var answer6 = 1 + Math.floor(Math.random() * 10); // random number between 1 and 10
+    console.log('This is my random number between 1 and 10: ' + answer6);
+    var numTries = 0;
+    var doneGame = false;
+    alert('Let\'s play a game. I am thinking of an integer between'
+                                  + '1 and 10. You have exactly four guesses to get the correct '
+                                  + 'answer. Let\'s go!');
+    while(numTries < 4) {
+      var response6 = parseInt(prompt('Type in your number'));
+      numTries++;
+      if (response6 === answer6) {
+        alert('You guessed right. That\'s awesome! Thank you for playing my game');
+        doneGame = true;
+        finalTally++;
+        break; // break out of the while loop
+      } else {
+        if (response6 > answer6) {
+          alert('Sorry, guess a lower number. You have ' + (4 - numTries) + ' tries left.');
+        }
+        else {
+          alert('Sorry, guess a higher number. You have ' + (4 - numTries) + ' tries left.');
+        }
       }
     }
-  }
 
-  if ((numTries === 4) && (!doneGame)) {
-    alert('Sorry, you exceeded the number of tries. Exiting game!');
-    numTries++;
+    if ((numTries === 4) && (!doneGame)) {
+      alert('Sorry, you exceeded the number of tries. Exiting game!');
+      numTries++;
+    }
   }
+  guessNumber();
 
   //------------------------
   //seventh question to user
   //------------------------
-  var answer7 = ['California', 'Michigan', 'Connecticut', 'Texas', 'Georgia'];
-  var numStateGuesses = 0;
-  var done = false;
+  function statesLived(){
+    var answer7 = ['California', 'Michigan', 'Connecticut', 'Texas', 'Georgia'];
+    var numStateGuesses = 0;
+    var done = false;
 
-  while(numStateGuesses < 6) {
-    var response7 = prompt('Can you guess a state I have lived in besides Washington?');
-    numStateGuesses++;
-    for (var i = 0; i < answer7.length; i++) {
-      if (response7 === answer7[i]){
-        alert('That\'s awesome! You got it right! Here are all the states I '
-             + 'lived in - ' + answer7);
-        done = true;
-        finalTally++;
-        break;  // break out of the for loop
+    while(numStateGuesses < 6) {
+      var response7 = prompt('Can you guess a state I have lived in besides Washington?');
+      numStateGuesses++;
+      for (var i = 0; i < answer7.length; i++) {
+        if (response7 === answer7[i]){
+          alert('That\'s awesome! You got it right! Here are all the states I '
+               + 'lived in - ' + answer7);
+          done = true;
+          finalTally++;
+          break;  // break out of the for loop
+        }
+      }
+      if (done) {
+        break; // break out of the while loop
+      } else {
+        alert('Sorry, guess again, you have ' + (6 - numStateGuesses) + ' tries left.');
       }
     }
-    if (done) {
-      break; // break out of the while loop
-    } else {
-      alert('Sorry, guess again, you have ' + (6 - numStateGuesses) + ' tries left.');
+    if ((numStateGuesses === 6) && (!done)) {
+      alert('Sorry, you exceeded the number of tries. Exiting game!');
+      alert('By the way, here are the other states I lived in - ' + answer7);
     }
   }
-  if ((numStateGuesses === 6) && (!done)) {
-    alert('Sorry, you exceeded the number of tries. Exiting game!');
-    alert('By the way, here are the other states I lived in - ' + answer7);
-  }
+  statesLived();
 
   // Reporting perf
 
