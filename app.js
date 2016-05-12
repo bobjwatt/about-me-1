f1c4809c23fbc959b00c9a9b2056eb3d4f64c965
TartarSauce <uma_bangalore@hotmail.com>
2016-05-11 20:19:38 -0700

    Added user performance statistics, removed some console.log statements

--- app.js
+++ app.js
@@ -4,6 +4,13 @@
 var userName = prompt('Hi, Thanks for visiting my site!, what is your name');
 alert('Nice to meet you,' + userName + '. I\'m going you ask you some questions about me.');
 console.log(userName);
+
+//------------------------
+// variables to keep statistics
+//------------------------
+var finalTally = 0;
+var totalQuestions = 7;
+
 //------------------------
 // First question to user
 //------------------------
@@ -14,6 +21,7 @@ var petBorder = false;
 
 if (response1 === answer1 || response1 === answer1[0]) {
   alert('You are correct, ' + userName);
+  finalTally++;
 } else {
   alert('You are not correct, ' + userName + '. I do have a dog');
 }
@@ -29,6 +37,7 @@ var homeTownBorder = false;
 
 if (response2.toLowerCase() === answer2) {
   alert('Yes beautiful Seattle, that\'s my hometown, ' + userName + '.');
+  finalTally++;
 } else {
   alert('No, sorry that is not correct. I am from Seattle in Washington.');
 }
@@ -44,6 +53,7 @@ var foodBorder = false;
 
 if (response3 === answer3) {
   alert('Yay! that\'s correct, ' + userName + '. I love pizza.');
+  finalTally++;
 } else {
   alert('No, sorry that is not correct. I love eating pizza.');
 }
@@ -59,6 +69,7 @@ var movieBorder = false;
 
 if (response4 === answer4) {
   alert('That movie gives me the creeps. Good guess, ' + userName + '.');
+  finalTally++;
 } else {
   alert('Actually, the movie that scared me the most recently was Poltergeist.');
 }
@@ -74,6 +85,7 @@ var favPlaceBorder = false;
 
 if (response5 === answer5) {
   alert('Yes, nothing like a staycation, ' + userName + '.');
+  finalTally++;
 } else {
   alert('No, I usually like to laze around at home.');
 }
@@ -87,7 +99,7 @@ console.log('This is my random number between 1 and 10: ' + answer6);
 var numTries = 0;
 var doneGame = false;
 alert('Let\'s play a game. I am thinking of an integer between'
-                              + '1 and 10. You have exactly four guesses to get the correct'
+                              + '1 and 10. You have exactly four guesses to get the correct '
                               + 'answer. Let\'s go!');
 while(numTries < 4) {
   var response6 = parseInt(prompt('Type in your number'));
@@ -95,6 +107,7 @@ while(numTries < 4) {
   if (response6 === answer6) {
     alert('You guessed right. That\'s awesome! Thank you for playing my game');
     doneGame = true;
+    finalTally++;
     break; // break out of the while loop
   } else {
     if (response6 > answer6) {
@@ -121,13 +134,12 @@ var done = false;
 while(numStateGuesses < 6) {
   var response7 = prompt('Can you guess a state I have lived in besides Washington?');
   numStateGuesses++;
-  console.log(numStateGuesses + 'Num guesses');
   for (var i = 0; i < answer7.length; i++) {
-    console.log(response7, answer7[i]);
     if (response7 === answer7[i]){
       alert('That\'s awesome! You got it right! Here are all the states I '
            + 'lived in - ' + answer7);
       done = true;
+      finalTally++;
       break;  // break out of the for loop
     }
   }
@@ -141,6 +153,11 @@ if ((numStateGuesses === 6) && (!done)) {
   alert('Sorry, you exceeded the number of tries. Exiting game!');
   alert('By the way, here are the other states I lived in - ' + answer7);
 }
+
+// Reporting perf
+
+alert(userName + ', you got ' + finalTally + ' questions right ' + ' out of ' + totalQuestions + '.');
+
 // // //------------------------
 // // draw borders around all images that showed up
 // //------------------------
